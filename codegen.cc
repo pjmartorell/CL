0fdf86f52449ac834390ebc70fdc28f6c1855ff9
Pere Joan Martorell <perejoan@itnig.net>
2010-12-13 18:28:15 +0100

    Passo fins a jp 27

--- codegen.cc
+++ codegen.cc
@@ -112,13 +112,33 @@ void CodeGenRealParams(AST *a,ptype tp,codechain &cpushparam,codechain &cremovep
 {
   if (!a) return;
   //cout<<"Starting with node \""<<a->kind<<"\""<<endl;
+	AST *param = child(child(a,1),0);
 
-  //...to be done.
-
+		tp = tp->down;
+		while(param)
+		{
+			if(tp->kind == "parref")
+			{
+				cpushparam = cpushparam || GenLeft(param, t) || "pushparam t" + itostring(t);
+			}
+			else if(tp->kind == "parval")
+			{
+				cpushparam = cpushparam || GenRight(param, t) || "pushparam t" + itostring(t);				
+			}
+			cremoveparam = cremoveparam || "killparam";
+			param = param->right;
+			tp = tp->right;
+		}
+		
+		int js = symboltable.jumped_scopes(child(a,0)->text);
+		//cout << "jumpedscopes = "+ itostring(js) + " || " + child(a,0)->text << endl;
+		cpushparam = cpushparam || indirections(js, t);
+		cremoveparam = cremoveparam || "killparam";
+		cpushparam = cpushparam || "pushparam t" + itostring(t);
+		
   //cout<<"Ending with node \""<<a->kind<<"\""<<endl;
 }
 
-// ...to be completed:
 codechain GenLeft(AST *a,int t)
 {
   codechain c;
@@ -128,8 +148,23 @@ codechain GenLeft(AST *a,int t)
   }
 
   //cout<<"Starting with node \""<<a->kind<<"\""<<endl;
-  if (a->kind=="ident") {
-    c="aload _"+a->text+" t"+itostring(t);
+  if (a->kind=="ident") 
+	{
+		int js = symboltable.jumped_scopes(a->text);	//Nombre de contextos saltats
+		if (js > 0) // Estic en un procediment, no al prog principal
+		{
+			c = "load static_link t" + itostring(t);
+			for (int i = 1; i < js; i++) c = c || "load t" + itostring(t) + " t" + itostring(t);
+			c = c || "addi t"+itostring(t)+" "+" offset(" + symboltable.idtable(a->text) + ":_" + a->text +") t"+itostring(t);
+		  if (symboltable[a->text].kind == "idparref") 
+				c = c || + "load t" + itostring(t) + " t" + itostring(t);
+		} 
+		else
+		{
+			if (symboltable[a->text].kind == "idparref" || !isbasickind(a->tp->kind) && symboltable[a->text].kind == "idparval")
+				c = "load _" + a->text + " t" + itostring(t);			
+			else c = "aload _" + a->text + " t" + itostring(t);		
+		}
   }
   else if (a->kind=="."){
     c=GenLeft(child(a,0),t)||
@@ -150,7 +185,6 @@ codechain GenLeft(AST *a,int t)
 }
 
 
-// ...to be completed:
 codechain GenRight(AST *a,int t)
 {
   codechain c;
@@ -163,12 +197,12 @@ codechain GenRight(AST *a,int t)
   if (a->ref) {
     if (a->kind=="ident" && symboltable.jumped_scopes(a->text)==0 &&
 	isbasickind(symboltable[a->text].tp->kind) && symboltable[a->text].kind!="idparref") {
-	c="load _"+a->text+" t"+itostring(t);
+	c="load _"+a->text+" t"+itostring(t);		//és una local var
     }
-    else if (isbasickind(a->tp->kind)) {
+    else if (isbasickind(a->tp->kind)) {	//parval of array/struct, or parref
       c=GenLeft(a,t)||"load t"+itostring(t)+" t"+itostring(t);
     }
-    else {//...to be done
+    else {		//local var or local parval of basic type/pointer
     }    
   } 
   else if (a->kind=="intconst") c="iload "+a->text+" t"+itostring(t);
@@ -221,6 +255,10 @@ codechain GenRight(AST *a,int t)
 		|| GenRight(child(a,1),t+1)
 		|| "land t"+itostring(t)+" t"+itostring(t+1)+" t"+itostring(t);		
 	}
+	else if (a->kind=="not") {
+		c=GenRight(child(a,0),t)
+		|| "lnot t"+itostring(t)+" t"+itostring(t);		
+	}
 	
   else {
     cout<<"BIG PROBLEM! No case defined for kind "<<a->kind<<endl;
@@ -232,7 +270,8 @@ codechain GenRight(AST *a,int t)
 // ...to be completed:
 codechain CodeGenInstruction(AST *a,string info="")
 {
-  codechain c;
+  codechain c, pushpars, killpars;
+
 
   if (!a) {
     return c;
@@ -257,7 +296,7 @@ codechain CodeGenInstruction(AST *a,string info="")
   } 
   else if (a->kind=="write" || a->kind=="writeln") {
     if (child(a,0)->kind=="string") {
-      //...to be done.
+			c = "wris " + child(a,0)->text; 
     } 
     else {//Exp
       c=GenRight(child(a,0),0)||"wrii t0";
@@ -294,9 +333,13 @@ codechain CodeGenInstruction(AST *a,string info="")
 			|| CodeGenInstruction(child(a,2),info)
 			|| "etiq endif_" + itostring(count);
 		}
-		
 	}
-	
+	else if (a->kind=="(") { //Crida a procediment
+		CodeGenRealParams(a, a->tp, pushpars, killpars, 0);
+		c = pushpars;
+		c = c || "call " + symboltable.idtable(child(a, 0)->text) + "_" + child(a, 0)->text;
+		c = c || killpars;
+	}
   //cout<<"Ending with node \""<<a->kind<<"\""<<endl;
 //	cout << a->kind << endl;
   return c;
@@ -305,15 +348,57 @@ codechain CodeGenInstruction(AST *a,string info="")
 void CodeGenSubroutine(AST *a,list<codesubroutine> &l)
 {
   codesubroutine cs;
-
+	int isfunc = 0;
+	
   //cout<<"Starting with node \""<<a->kind<<"\""<<endl;
   string idtable=symboltable.idtable(child(a,0)->text);
   cs.name=idtable+"_"+child(a,0)->text;
   symboltable.push(a->sc);
   symboltable.setidtable(idtable+"_"+child(a,0)->text);
 
-  //...to be done.
+	gencodevariablesandsetsizes(a->sc, cs, isfunc);
 
+	// ASTPrintIndent(a, "");
+	
+	for (AST *a1 = child(child(a,2), 0); a1 != 0; a1 = a1->right) {
+	    CodeGenSubroutine(a1, l);
+	  }
+
+	// Inicialitzem a 0 els ifs i whiles de la subrutina
+	newLabelIf(true); 
+	newLabelWhile(true);
+	
+	maxoffsetauxspace=0;
+		
+	cs.c = CodeGenInstruction(child(a,3));
+	
+	/*
+	if (isfunc)
+	{
+		// write_type(a->tp->down);
+		if (isbasickind(child(a, 4)->tp->kind)) // Retorn de Tipus Simple
+		{
+			cs.c = cs.c || GenRight(child(a, 4), 0);
+			cs.c = cs.c || "stor t0 returnvalue";					
+		}
+		else // Retorn de tipus Complex
+		{
+			cs.c = cs.c || GenLeft(child(a, 4), 1);
+			cs.c = cs.c || "load returnvalue t0" || "copy t1 t0 " + itostring(compute_size(child(a, 4)->tp));
+		}
+	}
+	*/
+	cs.c = cs.c || "retu";
+
+  if (maxoffsetauxspace > 0) 
+	{
+    variable_data vd;
+    vd.name = "aux_space";
+    vd.size = maxoffsetauxspace;
+    cs.localvariables.push_back(vd);
+  }
+
+  //
   symboltable.pop();
   l.push_back(cs);
   //cout<<"Ending with node \""<<a->kind<<"\""<<endl;
